02075048e5c65453f8430f214e1c1f57cdac3257
Jay Miller <kjaymiller@gmail.com>
2022-10-30 16:36:57 -0700

    initial commit

--- BadgeTopicsDeadline/__init__.py
+++ BadgeTopicsDeadline/__init__.py
@@ -1,6 +1,5 @@
 import json
 import logging
-from calendar import c
 
 import azure.functions as func
 
@@ -8,17 +7,19 @@ from shared.days_until import get_days_until
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    """Calculate days until next thu"""
+    """Calculate days until next wednesday"""
+    logging.info("Python HTTP trigger function processed a request.")
 
-    msg, color = get_days_until(5, time=(15, 0, 0))
+    msg, color = get_days_until(4)
+
     # return shield.io json
     return func.HttpResponse(
         json.dumps(
             {
                 "schemaVersion": 1,
-                "label": "Next Recording in",
+                "label": "Topic Deadline in",
                 "message": msg,
-                "color": color,
+                "color": "blue",
             }
         )
     )
